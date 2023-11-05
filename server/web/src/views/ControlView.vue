86984926ad7ccabf925c9bb9a545d385a2fb636a
mbatu <maxim.baturov@gmail.com>
2023-11-05 14:59:50 +0200

    update2

--- server/web/src/views/ControlView.vue
+++ server/web/src/views/ControlView.vue
@@ -1,11 +1,6 @@
 
 <template>
   <main>
-    <!-- <TheWelcome /> -->
-    <video id="video1" width="500" height="500" autoplay muted></video> <br />
-
-    Logs<br />
-    <div id="logs"></div>
   </main>
 </template>
 
@@ -71,73 +66,6 @@
         }
     });
 
-    function log(msg) {
-      document.getElementById('logs').innerHTML += msg + '<br>'
-    }
-
-    function startSession(sd) {
-      // const sd = document.getElementById('remoteSessionDescription').value
-      if (sd === '') {
-        return alert('Session Description must not be empty')
-      }
-
-      try {
-        pc.setRemoteDescription(JSON.parse(atob(sd)))
-      } catch (e) {
-        alert(e)
-      }
-    }
-
-    (function () {
-      const pc = new RTCPeerConnection({
-        iceServers: [
-          {
-            urls: 'stun:stun.l.google.com:19302'
-          }
-        ]
-      })
-      pc.oniceconnectionstatechange = e => log(pc.iceConnectionState)
-      pc.onicecandidate = event => {
-        if (event.candidate === null) {
-          let xhr = new XMLHttpRequest();
-          xhr.open("POST", `http://${host}:${port}/stream`);
-          xhr.setRequestHeader("Accept", "application/json");
-          xhr.setRequestHeader("Content-Type", "application/json");
-
-          xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-              console.log(xhr.status);
-              console.log(xhr.responseText);
-            }};
-
-          xhr.send(JSON.stringify({ "data": btoa(JSON.stringify(pc.localDescription)) }));
-
-        }
-      }
-
-      pc.addTransceiver('video')
-      pc.createOffer()
-          .then(d => pc.setLocalDescription(d))
-          .catch(log)
-
-      pc.ontrack = function (event) {
-        const el = document.getElementById('video1')
-        el.srcObject = event.streams[0]
-        el.autoplay = true
-        el.controls = true
-        el.style.width = "500px"
-        el.style.height = "500px"
-      }
-
-      // startSession()
-
-      // const btns = document.getElementsByClassName('createSessionButton')
-      // for (let i = 0; i < btns.length; i++) {
-      //   btns[i].style = 'display: none'
-      // }
-      //
-      // document.getElementById('signalingContainer').style = 'display: block'
-      //
-    })()
+    
 
 </script>
